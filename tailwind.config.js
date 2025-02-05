9bad325ea0940a12bc801450c41acaa0375ed620
Imants <imants.kokis@gmail.com>
2025-02-05 22:46:45 +0200

    feat: enhance typography and animations in Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const { text } = require('stream/consumers');
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
+  plugins: [
+    plugin(function({ addBase }) {
+     addBase({
+        'html': { fontSize: "20px", fontFamily: 'SequelSans, sans-serif'},
+        'h1': { fontSize: "3.815rem" },
+        'h2': { fontSize: "3.052rem" },
+        'h3': { fontSize: "2.441rem" },
+        'h4': { fontSize: "1.953rem" },
+        'h5': { fontSize: "1.563rem" },
+        'h6': { fontSize: "1.25rem" },
+        'p': { fontSize: "1.953rem", },
+        'ml': { fontSize: "0.8rem", textTransform: 'lowercase' }, 
+        'small': { fontSize: "1.15rem" },
+        'price': { fontSize: "1.15rem" },
+        'bold': { 
+          fontFamily: 'BoldBold, sans-serif',
+          wordSpacing: '-0.2em',
+          textTransform: 'uppercase',
+        },
+      })
+    }),
+  ],
   content: [
     './index.html',
     './src/**/*.{js,jsx,ts,tsx,vue,css}',
@@ -40,11 +65,20 @@ module.exports = {
           '50%': { transform: 'translateX(5px)' },
           '100%': { transform: 'translateX(0px)' },
         },
+        sliderEnter: {
+          '0%': { transform: 'translateX(10vw)' },
+          '100%': { transform: 'translateX(calc(-80% + 100vw)) translateY(-40%)' }
+        },
+        imgParallax: {
+          '0%': { objectPosition: '100% center' },
+          '100%': { objectPosition: '0% center' }
+        }
       },
       animation: {
         'backspin': 'backspin 3s cubic-bezier(0.3, -0.4, 0, 1.1) 1',
         'ping-slow': 'ping 5s cubic-bezier(0, -5, 0.25, 5) infinite',
-        'toycar': 'toycar 1s ease-in-out forwards',
+        'slider-scroll': 'sliderEnter linear forwards',
+        'img-parallax': 'imgParallax linear'
       },
     },
   },
